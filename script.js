94cc9a8cf8e251d12626bb538fb574dce9c9cdcb
clearxheaded <7777x777x7777@gmail.com>
2024-12-12 13:45:35 -0500

    Write the logic to play a single round
    - Added a new function that accepts user and computer choices and gives the result
    - Created 2 global variables for tracking scores

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     let choice = Math.random();
     if (choice <= 0.33) {
@@ -13,3 +17,20 @@ function getHumanChoice() {
     let userChoice = prompt("Enter your choice (rock, paper, scissors): ");
     return userChoice.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') 
+    || (humanChoice === 'paper' && computerChoice === 'rock') 
+    || (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        humanScore++;
+        return `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
+    }
+    else if (humanChoice === computerChoice) {
+        return `It's a tie! Play again.`;
+    }
+    computerScore++;
+    return `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
