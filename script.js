ab5b548d96d73251a5170c2f40b9cadaf1a4956f
clearxheaded <7777x777x7777@gmail.com>
2024-12-18 18:41:25 -0500

    Add 5 round functionality
    - Added a running score and logic to output results once one player reaches 5 points

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// function playGame() {
-//     let humanScore = 0;
-//     let computerScore = 0;
-//     function playRound(playerSelection, getComputerChoice()) {
-//         if ((playerSelection === 'rock' && getComputerChoice() === 'scissors') 
-//         || (playerSelection === 'paper' && getComputerChoice() === 'rock') 
-//         || (playerSelection === 'scissors' && getComputerChoice() === 'paper')) {
-//             humanScore++;
-//             return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${getComputerChoice().charAt(0).toUpperCase() + getComputerChoice().slice(1)}`;
-//         }
-//         else if (playerSelection === getComputerChoice()) {
-//             return `It's a tie! Play again.`;
-//         }
-//         computerScore++;
-//         return `You lose! ${getComputerChoice().charAt(0).toUpperCase() + getComputerChoice().slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
-//     }
-//     for (let counter = 0; counter < 5; counter++) {
-//         const humanSelection = getplayerSelection();
-//         const computerSelection = getgetComputerChoice()();
-//         console.log(playRound(humanSelection, computerSelection));
-//     }
-
-//     if (humanScore > computerScore) {
-//         console.log(`User's score: ${humanScore}, Computer's score: ${computerScore}. User wins!`);
-//     }
-//     else if (humanScore === computerScore) {
-//         console.log(`It's a tie!`);
-//     }
-//     else {
-//         console.log(`User's score: ${humanScore}, Computer's score: ${computerScore}. Computer wins!`)
-//     }
-// }
-
-// playGame();
-
 function getComputerChoice() {
     let choice = Math.random();
     if (choice < 0.33) {
@@ -44,19 +9,40 @@ function getComputerChoice() {
     return "scissors";
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, resultsDiv) {
     const computerSelection = getComputerChoice();
     let resultMessage = '';
     if ((playerSelection === 'rock' && computerSelection === 'scissors') 
         || (playerSelection === 'paper' && computerSelection === 'rock') 
         || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            playerScore++;
             resultMessage = `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
     } 
     else if (playerSelection === computerSelection) {
         resultMessage = `It's a tie! Play again.`;
     } 
     else {
+        computerScore++;
         resultMessage = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+    }
+
+    if (playerScore === 5) {
+        resultMessage = "You win! Congratulations!";
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore === 5) {
+        resultMessage = "Computer wins! Try again.";
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else {
+        resultMessage += `
+    Score:
+    Player: ${playerScore} Computer: ${computerScore}`;
     }
     resultsDiv.textContent = resultMessage;
 }
